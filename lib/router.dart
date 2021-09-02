f6905fe2b4525143a9551c21e8f151cc902abbc2
AdityaChaudhary18 <sanjords@gmail.com>
2021-09-02 15:11:44 +0530

    Adding warning in app

--- lib/router.dart
+++ lib/router.dart
@@ -2,7 +2,6 @@ import 'package:crews_net_app/Screens/Auth/login_view.dart';
 import 'package:crews_net_app/Screens/preloader.dart';
 import 'package:flutter/material.dart';
 
-import 'Screens/Auth/otp_verification_view.dart';
 import 'Screens/Auth/signup_view.dart';
 import 'Screens/Dashboard/dashboard_view.dart';
 import 'Screens/PageNotFound.dart';
