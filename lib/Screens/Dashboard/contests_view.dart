771d46e1b37f146df1357366c676753f54373401
Sanskar Modi <modisanskar5@gmail.com>
2021-09-04 15:48:16 +0530

    Contest Page made

--- lib/Screens/Dashboard/contests_view.dart
+++ lib/Screens/Dashboard/contests_view.dart
@@ -1,14 +1,214 @@
 import 'package:flutter/material.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
+import 'package:sizer/sizer.dart';
 import 'drawer.dart';
 
-class Contests extends StatelessWidget {
+class Contests extends StatefulWidget {
   const Contests({Key? key}) : super(key: key);
 
   @override
+  _ContestsState createState() => _ContestsState();
+}
+
+class _ContestsState extends State<Contests> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       drawer: DashDrawer(),
+      body: CustomScrollView(
+        physics: ScrollPhysics(),
+        shrinkWrap: true,
+        slivers: <Widget>[
+          SliverAppBar(
+            backgroundColor: Colors.black,
+            floating: true,
+            pinned: false,
+            snap: false,
+            title: Container(
+              width: 80.w,
+              height: 6.h,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(2.h),
+                border: Border.all(
+                  color: Color.fromRGBO(71, 71, 71, 1),
+                ),
+              ),
+              child: TextField(
+                cursorColor: Colors.blue[100],
+                keyboardType: TextInputType.text,
+                keyboardAppearance: Brightness.light,
+                textAlign: TextAlign.start,
+                decoration: InputDecoration(
+                  // enabled: false,
+                  border: OutlineInputBorder(borderSide: BorderSide.none),
+                  suffixIcon: Icon(
+                    FontAwesomeIcons.search,
+                    size: 3.5.h,
+                    color: Color.fromRGBO(188, 111, 241, 0.75),
+                  ),
+                  contentPadding: EdgeInsets.all(1.2.h),
+                  hintText: "Search..",
+                  hintStyle: TextStyle(
+                      color: Color.fromRGBO(188, 111, 241, 1),
+                      fontSize: 2.4.h),
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 1.2.h,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(1.h),
+                  child: Text(
+                    "Contests",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15.2.sp,
+                      color: Color.fromRGBO(188, 111, 241, 1),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 0.6.h,
+                ),
+              ],
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int item) {
+                return Card(
+                  margin: EdgeInsets.all(8.0),
+                  elevation: 2.0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0)),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        right: 10,
+                        bottom: 140,
+                        child: SavedIcon(),
+                      ),
+                      ListTile(
+                        autofocus: true,
+                        focusColor: Colors.lightGreen,
+                        hoverColor: Colors.lightGreen,
+                        contentPadding: EdgeInsets.symmetric(
+                          vertical: 15.0,
+                          horizontal: 15.0,
+                        ),
+                        title: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Text(
+                                  "26",
+                                  style: TextStyle(
+                                      fontSize: 38.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "JUNE ",
+                                  style: TextStyle(fontSize: 18.0),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        subtitle: Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "Codechef",
+                                    style: TextStyle(
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white),
+                                  ),
+                                  Text(
+                                    "7AM-8PM",
+                                    style: TextStyle(
+                                        fontSize: 20.0, color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "www.codechef.com",
+                                      style: TextStyle(fontSize: 18.0),
+                                    ),
+                                    Text(
+                                      "Active",
+                                      style: TextStyle(fontSize: 18.0),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              childCount: 8,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SavedIcon extends StatefulWidget {
+  const SavedIcon({Key? key}) : super(key: key);
+
+  @override
+  _SavedIconState createState() => _SavedIconState();
+}
+
+class _SavedIconState extends State<SavedIcon> {
+  bool _isSaved = false;
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        setState(() {
+          _isSaved = !_isSaved;
+          print(!_isSaved);
+        });
+      },
+      icon: Icon(
+        FontAwesomeIcons.bookmark,
+        color: _isSaved ? Colors.blue : Colors.white,
+        size: 40,
+      ),
     );
   }
 }
--- lib/Screens/Dashboard/dashboard_view.dart
+++ lib/Screens/Dashboard/dashboard_view.dart
@@ -22,127 +22,127 @@ class _DashboardState extends State<Dashboard> {
     var formatter = new DateFormat('dd-MM-yyyy');
     String formattedDate = formatter.format(now);
 
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.black,
-        drawer: DashDrawer(),
-        body: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              backgroundColor: Colors.black,
-              floating: true,
-              pinned: false,
-              snap: false,
-              title:  Center(
-                child: Container(
-                  width: 80.w,
-                  height: 6.h,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.rectangle,
-                    color: Colors.transparent,
-                    borderRadius: BorderRadius.circular(2.h),
-                    border: Border.all(
-                      color: Color.fromRGBO(71, 71, 71, 1),
-                    ),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      drawer: DashDrawer(),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            backgroundColor: Colors.black,
+            floating: true,
+            pinned: false,
+            snap: false,
+            title: Container(
+              width: 80.w,
+              height: 6.h,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(2.h),
+                border: Border.all(
+                  color: Color.fromRGBO(71, 71, 71, 1),
+                ),
+              ),
+              child: TextField(
+                cursorColor: Colors.blue[100],
+                keyboardType: TextInputType.text,
+                keyboardAppearance: Brightness.light,
+                textAlign: TextAlign.start,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
                   ),
-                  child: TextField(
-                    textAlign: TextAlign.start,
-                    decoration: InputDecoration(
-
-                      // enabled: false,
-                      suffixIcon: Icon(
-                        FontAwesomeIcons.search,
-                        size: 3.5.h,
-                        color: Color.fromRGBO(188, 111, 241, 1),
-                      ),
-                      contentPadding: EdgeInsets.all(1.2.h),
-                      hintText: "Search..",
-                      hintStyle: TextStyle(
-                          color: Color.fromRGBO(188, 111, 241, 1),
-                          fontSize: 2.4.h),
-                    ),
+                  // enabled: false,
+                  suffixIcon: Icon(
+                    FontAwesomeIcons.search,
+                    size: 3.5.h,
+                    color: Color.fromRGBO(188, 111, 241, 0.75),
                   ),
+                  contentPadding: EdgeInsets.all(1.2.h),
+                  hintText: "Search..",
+                  hintStyle: TextStyle(
+                      color: Color.fromRGBO(188, 111, 241, 1),
+                      fontSize: 2.4.h),
                 ),
               ),
             ),
-            SliverToBoxAdapter(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-
-                  SizedBox(
-                    height: 2.4.h,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(1.h),
-                    child: Text(
-                      "Feeds",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15.2.sp,
-                        color: Color.fromRGBO(188, 111, 241, 1),
-                      ),
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 1.2.h,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(1.h),
+                  child: Text(
+                    "Feeds",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15.2.sp,
+                      color: Color.fromRGBO(188, 111, 241, 1),
                     ),
                   ),
-                  SizedBox(
-                    height: 0.6.h,
-                  ),
-                ],
-              ),
+                ),
+                SizedBox(
+                  height: 0.6.h,
+                ),
+              ],
             ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-                  return Padding(
-                    padding: EdgeInsets.symmetric(vertical: 1.h),
-                    child: ListTile(
-                      contentPadding: EdgeInsets.symmetric(
-                          vertical: 1.8.h, horizontal: 4.w),
-                      tileColor: Color.fromRGBO(71, 71, 71, 1),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(3.6.h),
-                        side: BorderSide(color: Colors.black),
-                      ),
-                      leading: CircleAvatar(
-                        radius: 19.sp,
-                        child: Text(
-                          "S",
-                          style: TextStyle(
-                            fontSize: 15.2.sp,
-                            color: Colors.blue,
-                          ),
-                        ),
-                        backgroundColor: Colors.pink,
-                      ),
-                      trailing: IconButton(
-                        icon: Icon(
-                          Icons.more_vert_sharp,
-                          size: 3.6.h,
-                        ),
-                        onPressed: () {},
-                      ),
-                      title: Row(
-                        children: [
-                          Text("Sanskar"),
-                          SizedBox(
-                            width: 10.w,
-                          ),
-                          Text("${formattedDate}"),
-                        ],
-                      ),
-                      subtitle: GestureDetector(
-                        child: Text("Add a project"),
-                      ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(vertical: 1.h),
+                  child: ListTile(
+                    contentPadding: EdgeInsets.symmetric(
+                        vertical: 1.8.h, horizontal: 4.w),
+                    tileColor: Color.fromRGBO(71, 71, 71, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(3.6.h),
+                      side: BorderSide(color: Colors.black),
                     ),
-                  );
-                },
-                childCount: 8,
-              ),
+                    leading: CircleAvatar(
+                      radius: 19.sp,
+                      child: Text(
+                        "S",
+                        style: TextStyle(
+                          fontSize: 17.sp,
+                          color: Colors.green[900],
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      backgroundColor: Colors.pink,
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(
+                        Icons.more_vert_sharp,
+                        size: 3.6.h,
+                      ),
+                      onPressed: () {},
+                    ),
+                    title: Row(
+                      children: [
+                        Text("Sanskar"),
+                        SizedBox(
+                          width: 18.w,
+                        ),
+                        Text("$formattedDate"),
+                      ],
+                    ),
+                    subtitle: GestureDetector(
+                      child: Text("Add a project"),
+                    ),
+                  ),
+                );
+              },
+              childCount: 8,
             ),
-          ],
-        ),
-
+          ),
+        ],
       ),
     );
   }
