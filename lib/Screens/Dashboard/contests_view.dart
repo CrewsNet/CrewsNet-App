917455cbb0ded7770d04b110e8d0db9683d88230
Sanskar Modi <modisanskar5@gmail.com>
2021-09-07 00:45:01 +0530

    Further refractoring

--- lib/Screens/Dashboard/contests_view.dart
+++ lib/Screens/Dashboard/contests_view.dart
@@ -45,34 +45,7 @@ class _ContestsState extends State<Contests> {
     super.initState();
   }
 
-  String readTimestamp(int timestamp) {
-    var now = DateTime.now();
-    var format = DateFormat('HH:mm a');
-    var date = DateTime.fromMillisecondsSinceEpoch(timestamp * 1000);
-    var diff = now.difference(date);
-    var time = '';
 
-    if (diff.inSeconds <= 0 ||
-        diff.inSeconds > 0 && diff.inMinutes == 0 ||
-        diff.inMinutes > 0 && diff.inHours == 0 ||
-        diff.inHours > 0 && diff.inDays == 0) {
-      time = format.format(date);
-    } else if (diff.inDays > 0 && diff.inDays < 7) {
-      if (diff.inDays == 1) {
-        time = diff.inDays.toString() + ' DAY AGO';
-      } else {
-        time = diff.inDays.toString() + ' DAYS AGO';
-      }
-    } else {
-      if (diff.inDays == 7) {
-        time = (diff.inDays / 7).floor().toString() + ' WEEK AGO';
-      } else {
-        time = (diff.inDays / 7).floor().toString() + ' WEEKS AGO';
-      }
-    }
-
-    return time;
-  }
 
   getCustomFormattedDateTime(String givenDateTime, String dateFormat) {
     // dateFormat = 'MM/dd/yy';
@@ -187,7 +160,7 @@ class _ContestsState extends State<Contests> {
                         ),
                       ),
                       Container(
-                        height: 23.h,
+                        height: 23.5.h,
                         padding: EdgeInsets.symmetric(
                             vertical: 15, horizontal: 3.8.w),
                         child: Column(
