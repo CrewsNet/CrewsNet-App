0ed353e236da69b71fc957fc11009b7267509454
Sanskar Modi <modisanskar5@gmail.com>
2021-09-09 14:24:46 +0530

    Profile Page

--- lib/Screens/Dashboard/profile_view.dart
+++ lib/Screens/Dashboard/profile_view.dart
@@ -1,10 +1,22 @@
 import 'package:crews_net_app/Screens/Dashboard/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:sizer/sizer.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
+
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  late double _rating;
+  void initState() {
+    super.initState();
+    _rating = 3;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,6 +64,87 @@ class Profile extends StatelessWidget {
               ),
             ),
           ),
+          SliverToBoxAdapter(
+            child: Container(
+              height: 55.h,
+              margin: EdgeInsets.fromLTRB(0, 3.h, 0, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.blue,
+                    radius: 10.h,
+                  ),
+                  Text(
+                    ' $_rating',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.sp,
+                      color: Colors.green[500],
+                    ),
+                  ),
+                  RatingBar.builder(
+                    initialRating: 3,
+                    minRating: 1,
+                    direction: Axis.horizontal,
+                    allowHalfRating: true,
+                    glowColor: Colors.amber,
+                    glow: true,
+                    glowRadius: 25.sp,
+                    unratedColor: Colors.green[50],
+                    itemCount: 5,
+                    itemSize: 22.sp,
+                    itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                    itemBuilder: (context, _) => Icon(
+                      Icons.star,
+                      color: Colors.green[500],
+                    ),
+                    onRatingUpdate: (rating) {
+                      setState(
+                        () {
+                          _rating = rating;
+                        },
+                      );
+                    },
+                    updateOnDrag: true,
+                  ),
+                  Container(
+                    height: 18.h,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Sanskar Modi",
+                          style: TextStyle(
+                              fontSize: 20.sp, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "App Developer",
+                          style: TextStyle(
+                              fontSize: 15.sp, fontWeight: FontWeight.w300),
+                        ),
+                        Text(
+                          "modisanskar5@gmail.com",
+                          style: TextStyle(
+                              fontSize: 15.sp, fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          "Indian Institute of Information Technology, Sonepat",
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 12.sp, fontWeight: FontWeight.w700),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
--- lib/router.dart
+++ lib/router.dart
@@ -15,7 +15,7 @@ class RouteGenerator {
     final args = settings.arguments;
     switch (settings.name) {
       case '/':
-        return MaterialPageRoute(builder: (_) => Preloader());
+        return MaterialPageRoute(builder: (_) => Contests());
       case '/signup':
         return MaterialPageRoute(builder: (_) => SignUpPage());
       case '/dashboard':
